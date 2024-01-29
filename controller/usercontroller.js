e10e66602f1690fa70902795b1a0215ad3c33ef3
fazal <fazalahamed628@gmail.com>
2024-01-29 23:29:46 +0530

    initial

--- controller/usercontroller.js
+++ controller/usercontroller.js
@@ -67,9 +67,7 @@ req.session.user = {
     referralCode:user.referralCode
 };
 await cartItemCountMiddleware(req, res, () => {});
-const products = await Product.find().exec();
-const Rateus=await RateUs.find()
-            return res.render('./user/userhome', { user ,products,Rateus});
+res.redirect('/user/home');
         } else {
             req.flash('error1', 'Invalid email or password');
             return res.render('./user/login', { error1: req.flash('error1') });
